319dba66183c01f0c0266b9eb8a92e0a4e57c721
Vlad <vlad.kosevichhhh@gmail.com>
2023-06-27 16:24:58 +0300

    edit: settings config

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+
+const twColors = require('tailwindcss/colors')
+
+const colors = {
+  transparent: twColors.transparent,
+  black: '#191B1D',
+  white: '#fff',
+  primary: '#CDCECF',
+  secondary: '#BDA05F',
+
+}
+
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
