80624e6cdf65dfdb5c354dcc4c2707ad186a4ffa
Vlad <vlad.kosevichhhh@gmail.com>
2023-07-14 16:20:19 +0300

    start search panel

--- src/components/ui/common/search/Search.tsx
+++ src/components/ui/common/search/Search.tsx
@@ -1,7 +1,22 @@
+import { useQuery } from '@tanstack/react-query'
+import { useRouter } from 'next/router'
 import { FC } from 'react'
 
+import { ProductService } from '@/services/product/product.service'
+
 const Search: FC = () => {
-	return <></>
+	const { query } = useRouter()
+
+	const { data } = useQuery(['search products', query.term], () =>
+		ProductService.getAll({
+			searchTerm: query.term as string
+		})
+	)
+	return (
+		<>
+			<div></div>
+		</>
+	)
 }
 
 export default Search
