2d6f7410c56077055cb1437c2f1e4da485e581d0
Vlad <vlad.kosevichhhh@gmail.com>
2023-07-17 16:38:16 +0300

    create home paralax layout

--- src/components/screens/home/Home.tsx
+++ src/components/screens/home/Home.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import { FC } from 'react'
 import Scrollbars from 'react-custom-scrollbars'
 import 'react-html5video/dist/styles.css'
+import ReactPlayer from 'react-player'
 
 import Meta from '@/ui/Meta'
 import Layout from '@/ui/layout/Layout'
@@ -10,15 +11,14 @@ import Layout from '@/ui/layout/Layout'
 import { TypePaginationProducts } from '@/types/product.interface'
 
 import style from './home.module.scss'
+import testImg from './img/test-1.png'
 import Logo from './svg/logo.svg'
 import LowbarBtn from './svg/lowbar.svg'
-import videoBg from './video/video.png'
 
 const Home: FC<TypePaginationProducts> = ({ products, length }) => {
 	return (
 		<Scrollbars
-			thumbSize={200}
-			// autoHide
+			thumbSize={400}
 			style={{
 				height: '100vh'
 			}}
@@ -26,17 +26,22 @@ const Home: FC<TypePaginationProducts> = ({ products, length }) => {
 			<Meta title='Home'>
 				<Layout>
 					<section className={style.home}>
-						<Parallax pages={2} style={{ top: '0', left: '0' }}>
-							<ParallaxLayer offset={0} speed={0.3}>
+						<Parallax pages={3} style={{ top: '0', left: '0' }}>
+							<ParallaxLayer offset={0} speed={0.2}>
 								<div className={style.video}>
-									<Image
-										src={videoBg} // Путь к изображению в папке public
-										alt='My Image'
+									<ReactPlayer
+										muted
+										width={'70'}
+										height={'60vw'}
+										url={
+											'https://player.vimeo.com/video/726622515?h=557969d636'
+										}
+										controls={false}
+										playing={true}
 									/>
-									{/* <ReactPlayer url='https://i.vimeocdn.com/video/1675289256-6db006c7748147413dbd61e672aa747cfa1099e641538eab7bd0fc3f9bf3f7c6-d?mw=1920&mh=1080' /> */}
 								</div>
 							</ParallaxLayer>
-							<ParallaxLayer offset={0} speed={0.6}>
+							<ParallaxLayer offset={0} speed={0.7}>
 								<div className={style.homeGreyBg}>
 									<ParallaxLayer offset={0} speed={0.2}>
 										<div className='container-f'>
@@ -59,6 +64,46 @@ const Home: FC<TypePaginationProducts> = ({ products, length }) => {
 										</div>
 									</ParallaxLayer>
 								</div>
+								<ParallaxLayer offset={1.5} speed={0.1}>
+									<div className={style.category}>
+										<div className={`${style.setup} container-f`}>
+											<div className='btnWrapper'>
+												<div>See more</div>
+											</div>
+											<div className={style.categoryName}>
+												<h2>Ready setup</h2>
+												<h3>
+													Upgrade your workspace with our collection of desk
+													accessories! We offer a wide range of stylish and
+													functional products, including organizers, pen
+													holders, phone stands, desk lamps, portable chargers,
+													and much more.
+												</h3>
+											</div>
+											<div className={style.item}>
+												<Image src={testImg} alt={'img'}></Image>
+												<div className={style.descr}>
+													<h3>White Style SetUp</h3>
+													<h4>
+														A white modern workstation epitomizes elegance and
+														functionality. It features clean lines and
+														minimalist design, with ample space and storage.
+													</h4>
+												</div>
+											</div>
+											<div className={style.item}>
+												<Image src={testImg} alt={'img'}></Image>
+												<h3>Warm Style SetUp</h3>
+												<h4>
+													A warm, modern workstation with wood tones exudes a
+													cozy and inviting atmosphere. It combines sleek
+													contemporary design with natural elements, creating a
+													harmonious balance.
+												</h4>
+											</div>
+										</div>
+									</div>
+								</ParallaxLayer>
 							</ParallaxLayer>
 						</Parallax>
 						<div className={`${style.lowbar} container-f`}>
