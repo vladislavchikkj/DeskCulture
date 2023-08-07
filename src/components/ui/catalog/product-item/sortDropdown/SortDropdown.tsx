9e67a9060600fef213ef7f32ffae175e40ea5cdb
Vlad <vlad.kosevichhhh@gmail.com>
2023-08-07 16:19:09 +0300

    dropdown layout

--- src/components/ui/catalog/product-item/sortDropdown/SortDropdown.tsx
+++ src/components/ui/catalog/product-item/sortDropdown/SortDropdown.tsx
@@ -1,5 +1,6 @@
 import { Dispatch, FC, SetStateAction } from 'react'
 
+import style from './sortDropdown.module.scss'
 import { EnumProductSort } from '@/services/product/product.types'
 
 interface ISortDropdown {
@@ -13,6 +14,7 @@ const SortDropdown: FC<ISortDropdown> = ({ setSortType, sortType }) => {
 			<select
 				value={sortType}
 				onChange={e => setSortType(e.target.value as any)}
+				className={style.sortDropdown}
 			>
 				{(
 					Object.keys(EnumProductSort) as Array<keyof typeof EnumProductSort>
@@ -22,6 +24,7 @@ const SortDropdown: FC<ISortDropdown> = ({ setSortType, sortType }) => {
 							key={EnumProductSort[key]}
 							onChange={() => setSortType(EnumProductSort[key])}
 							value={EnumProductSort[key]}
+							className={style.x}
 						>
 							{EnumProductSort[key]}
 						</option>
