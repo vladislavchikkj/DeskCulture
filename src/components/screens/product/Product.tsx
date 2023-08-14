ef57e643e93ca146a1101fb563fea05e4bbddb1a
Vlad <vlad.kosevichhhh@gmail.com>
2023-08-14 16:09:53 +0300

    strt product page

--- src/components/screens/product/Product.tsx
+++ src/components/screens/product/Product.tsx
@@ -3,10 +3,14 @@ import { FC } from 'react'
 import Meta from '@/ui/Meta'
 import Layout from '@/ui/layout/Layout'
 
+import style from './product.module.scss'
+
 const Product: FC = () => {
 	return (
 		<Meta title='Product'>
-			<Layout inView={false} children={undefined}></Layout>
+			<Layout inView={false}>
+				<div className={style.product}>Name</div>
+			</Layout>
 		</Meta>
 	)
 }
