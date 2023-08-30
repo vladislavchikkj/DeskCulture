24f72e22a3064eba7ff5e0569a38742edbab1c6d
Vlad <vlad.kosevichhhh@gmail.com>
2023-08-30 16:22:23 +0300

    add order service

--- src/services/order.service.ts
+++ src/services/order.service.ts
@@ -1,14 +1,34 @@
 import { instance } from '@/api/api.interceptor'
+import { ICartItem } from '@/types/cart.interface'
 
 import { IOrder } from './order.interface'
 
 const ORDERS = 'orders'
+
+enum EnumOrderStatus {
+	PENDING = 'PENDING',
+	PAYED = 'PAYED',
+	SHIPPED = 'SHIPPED',
+	DELIVERED = 'DELIVERED'
+}
+
+type TypeData = {
+	status?: EnumOrderStatus
+	items: Pick<ICartItem, 'price' | 'quantity'> & { productId: number }
+}
 
 export const OrderService = {
 	async getAll() {
 		return instance<IOrder[]>({
 			url: ORDERS,
 			method: 'GET'
+		})
+	},
+	async place(data: TypeData) {
+		return instance<{ confirmation: { confirmationUrl: string } }>({
+			url: ORDERS,
+			method: 'POST',
+			data
 		})
 	}
 }
