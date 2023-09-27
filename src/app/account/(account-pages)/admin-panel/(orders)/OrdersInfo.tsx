48d438536531e8e664921fd418188901ee33a238
Vlad <vlad.kosevich@mail.ru>
2023-09-27 18:34:28 +0300

    add structure component

---
+++ src/app/account/(account-pages)/admin-panel/(orders)/OrdersInfo.tsx
@@ -0,0 +1,9 @@
+'use client'
+import { FC } from 'react'
+import style from './orders.module.scss'
+
+const OrdersInfo: FC = () => {
+	return <div className={style.orders}></div>
+}
+
+export default OrdersInfo
--- src/app/account/(account-pages)/admin-panel/page.tsx
+++ src/app/account/(account-pages)/admin-panel/page.tsx
@@ -1,11 +1,13 @@
 'use client'
-import { useAdmin } from '@/hooks/useAdmin'
 import { Metadata } from 'next'
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import Account from '../Account'
 import accountStyle from '../account.module.scss'
+import CatalogSettings from './(catalog)/CatalogSettings'
+import Dashboard from './(dashboard)/Dashboard'
 import Users from './(users)/Users'
 import style from './admin-panel.module.scss'
+import OrdersInfo from './(orders)/OrdersInfo'
 
 export const metadata: Metadata = {
 	description: 'DeskCulture',
@@ -13,19 +15,35 @@ export const metadata: Metadata = {
 }
 
 const AdminPanel: FC = () => {
+	const [selectedTab, setSelectedTab] = useState<
+		'dashboard' | 'users' | 'catalog' | 'orders'
+	>('dashboard')
+
+	const renderContent = () => {
+		switch (selectedTab) {
+			case 'dashboard':
+				return <Dashboard />
+			case 'users':
+				return <Users />
+			case 'catalog':
+				return <CatalogSettings />
+			case 'orders':
+				return <OrdersInfo />
+			default:
+				return null
+		}
+	}
+
 	return (
 		<Account>
 			<h1 className={accountStyle.title}>Admin panel</h1>
 			<div className={style.menu}>
-				<div>Dashboard</div>
-				<div>Users</div>
-				<div>Catalog</div>
-				<div>Orders</div>
+				<div onClick={() => setSelectedTab('dashboard')}>Dashboard</div>
+				<div onClick={() => setSelectedTab('users')}>Users</div>
+				<div onClick={() => setSelectedTab('catalog')}>Catalog</div>
+				<div onClick={() => setSelectedTab('orders')}>Orders</div>
 			</div>
-			<div className={style.content}>
-				{/* <Dashboard /> */}
-				<Users />
-			</div>
+			<div className={style.content}>{renderContent()}</div>
 		</Account>
 	)
 }
