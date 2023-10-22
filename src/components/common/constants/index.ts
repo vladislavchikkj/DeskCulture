01984982d6b33eb4ccccee73d546ec46c1d547f7
Vlad <vlad.kosevich@mail.ru>
2023-10-23 01:17:20 +0300

    add config

--- src/components/common/constants/index.ts
+++ src/components/common/constants/index.ts
@@ -1,4 +1,4 @@
 // export * from './constants'
 export const welcomeLogin = 'welcome, Sign in'
 export const welcomeRegister = 'Welcome,	create an account'
-export const serverAddress = 'http://localhost:4200'
+export const serverAddress = 'https://deskculture-server.onrender.com'
