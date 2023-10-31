eafde9cf86c92ec370d942d030e8a7721b246688
Vlad <vlad.kosevichhhh@gmail.com>
2023-10-31 13:50:46 +0300

    add new v next

---
+++ src/app/catalog/(dynamic)/categories/[slug]/not-found.tsx
@@ -0,0 +1,17 @@
+import Button from '@/ui/common/buttons/Button'
+import Heading from '@/ui/common/heading/Heading'
+import Link from 'next/link'
+
+export default function NotFound() {
+	return (
+		<div className='container-f h-screen flex items-center justify-center flex-col'>
+			<Heading variant='auth'>Not Found Page</Heading>
+			<div>Could not find requested resource</div>
+			<Link href='/'>
+				<Button data-hover='Go home' variant='grey' className='mt-5'>
+					Go home
+				</Button>
+			</Link>
+		</div>
+	)
+}
