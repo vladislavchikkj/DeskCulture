e9e7d562192a900b4ad35cf589bee34616e2d7f7
Vlad <vlad.kosevichhhh@gmail.com>
2023-11-08 13:39:52 +0300

    DELET META

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,60 +6,58 @@ import { SetupsService } from '@/services/setups.service'
 import type { Metadata } from 'next'
 import Home from './home/Home'
 
-export const metadata: Metadata = {
-	icons: {
-		icon: './favicon.ico',
-		shortcut: './favicon.ico',
-		apple: './favicon.ico',
-		other: {
-			rel: './favicon.ico',
-			url: './favicon.ico'
-		}
-	},
-	title: {
-		absolute: `Create Your Workspace | DeskCulture`,
-		template: `%s | DeskCulture`
-	},
-	description: `Create a workspace that epitomizes your refined preferences while
-	igniting your drive for productivity.`,
-	metadataBase: new URL(getSiteUrl()),
-	openGraph: {
-		type: 'website',
-		siteName: 'DeskCulture',
-		emails: ['desk.culture.official@gmail.com'],
-		images: INTRO_IMG_PATH
-	},
-	keywords: [
-		'desk',
-		'culture',
-		'desk culture',
-		'Desk-Culture',
-		'desk-culture',
-		'Create Your Workspace'
-	],
-	twitter: {
-		card: 'app',
-		title: `DeskCulture`,
-		description:
-			'Create a workspace that epitomizes your refined preferences while igniting your drive for productivity.',
-		creator: 'jsCreator',
-		images: INTRO_IMG_PATH,
-		app: {
-			name: 'DeskCulture',
-			id: {
-				iphone: 'twitter_app://iphone',
-				ipad: 'twitter_app://ipad',
-				googleplay: 'twitter_app://googleplay'
-			},
-			url: {
-				iphone: 'https://iphone_url',
-				ipad: 'https://ipad_url'
-			}
-		}
-	}
-}
-
-export const revalidate = 60
+// export const metadata: Metadata = {
+// 	icons: {
+// 		icon: './favicon.ico',
+// 		shortcut: './favicon.ico',
+// 		apple: './favicon.ico',
+// 		other: {
+// 			rel: './favicon.ico',
+// 			url: './favicon.ico'
+// 		}
+// 	},
+// 	title: {
+// 		absolute: `Create Your Workspace | DeskCulture`,
+// 		template: `%s | DeskCulture`
+// 	},
+// 	description: `Create a workspace that epitomizes your refined preferences while
+// 	igniting your drive for productivity.`,
+// 	metadataBase: new URL(getSiteUrl()),
+// 	openGraph: {
+// 		type: 'website',
+// 		siteName: 'DeskCulture',
+// 		emails: ['desk.culture.official@gmail.com'],
+// 		images: INTRO_IMG_PATH
+// 	},
+// 	keywords: [
+// 		'desk',
+// 		'culture',
+// 		'desk culture',
+// 		'Desk-Culture',
+// 		'desk-culture',
+// 		'Create Your Workspace'
+// 	],
+// 	twitter: {
+// 		card: 'app',
+// 		title: `DeskCulture`,
+// 		description:
+// 			'Create a workspace that epitomizes your refined preferences while igniting your drive for productivity.',
+// 		creator: 'jsCreator',
+// 		images: INTRO_IMG_PATH,
+// 		app: {
+// 			name: 'DeskCulture',
+// 			id: {
+// 				iphone: 'twitter_app://iphone',
+// 				ipad: 'twitter_app://ipad',
+// 				googleplay: 'twitter_app://googleplay'
+// 			},
+// 			url: {
+// 				iphone: 'https://iphone_url',
+// 				ipad: 'https://ipad_url'
+// 			}
+// 		}
+// 	}
+// }
 
 async function getDataInfo() {
 	const categories = await CategoryService.getAll()
