fbb79ba7e538c4535883982abc0c21a229756631
Vlad <vlad.kosevichhhh@gmail.com>
2023-11-08 15:35:58 +0300

    change btn add to cart

--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -1,4 +1,3 @@
-import { getSiteUrl } from '@/config/url.config'
 import { SITE_NAME } from '@/constants/app.constants'
 import { ICON_PATH } from '@/constants/favicon.constant'
 import Catalog from '@/ui/catalog/Catalog'
@@ -12,7 +11,6 @@ export const metadata: Metadata = {
 		absolute: `Create Your Workspace | Catalog`,
 		template: `%s | Catalog`
 	},
-	metadataBase: new URL(getSiteUrl()),
 	openGraph: {
 		type: 'website',
 		siteName: SITE_NAME,
