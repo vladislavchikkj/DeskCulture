5d57d906d5197397adfb3a688687a33325f5543c
Vlad <vlad.kosevichhhh@gmail.com>
2023-11-10 11:50:22 +0300

    update video bg

--- src/app/home/components/player/homePlayer.tsx
+++ src/app/home/components/player/homePlayer.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react'
-import style from './homeplayer.module.scss'
 import ReactPlayer from 'react-player'
+import style from './homeplayer.module.scss'
 //@ts-ignore
 
 const HomePlayer: FC = () => {
